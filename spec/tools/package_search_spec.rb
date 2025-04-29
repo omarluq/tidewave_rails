bdf3e5caab073016a7b5483c8ac8f009a7d79abb
Omar Luq <84993125+omarluq@users.noreply.github.com>
2025-04-29 15:13:37 -0500

    drop faraday

--- spec/tools/package_search_spec.rb
+++ spec/tools/package_search_spec.rb
@@ -18,11 +18,9 @@ describe Tidewave::Tools::PackageSearch do
       expect(described_class.description).to eq(
         <<~DESCRIPTION
           Searches for packages on RubyGems.
-
           Use this tool if you need to find new packages to add to the project. Before using this tool,
           get an overview of the existing dependencies by using the `project_eval` tool and executing
           `Gem::Specification.map { |gem| [gem.name, gem.version] }`.
-
           The results are paginated, with 30 packages per page. Use the `page` parameter to fetch a specific page.
         DESCRIPTION
       )
@@ -55,34 +53,48 @@ describe Tidewave::Tools::PackageSearch do
 
   describe "#call" do
     let(:response_body) { File.read("spec/fixtures/package_search.json") }
+    let(:http_response) { instance_double('Net::HTTPResponse') }
 
     context "without page" do
       it "returns results for page 1" do
-        faraday_double = instance_double('Faraday::Connection')
-        response = instance_double('Faraday::Response')
-        expect(faraday_double).to receive(:get).with("/api/v1/search.json?query=rails&page=1").and_return(response)
-        expect(response).to receive(:body).and_return(response_body)
+        uri = URI("https://rubygems.org/api/v1/search.json")
+        uri.query = URI.encode_www_form(query: "rails", page: 1)
 
-        expect(Faraday).to receive(:new).and_return(faraday_double)
+        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(http_response)
+        expect(http_response).to receive(:is_a?).with(Net::HTTPSuccess).and_return(true)
+        expect(http_response).to receive(:body).and_return(response_body)
 
         result = described_class.new.call(search: "rails")
-
-        expect(result).to eq(response_body)
+        expect(result).to eq(JSON.parse(response_body))
       end
     end
 
     context "with page" do
       it "returns results for the given page" do
-        faraday_double = instance_double('Faraday::Connection')
-        response = instance_double('Faraday::Response')
-        expect(faraday_double).to receive(:get).with("/api/v1/search.json?query=rails&page=2").and_return(response)
-        expect(response).to receive(:body).and_return(response_body)
+        uri = URI("https://rubygems.org/api/v1/search.json")
+        uri.query = URI.encode_www_form(query: "rails", page: 2)
 
-        allow(Faraday).to receive(:new).and_return(faraday_double)
+        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(http_response)
+        expect(http_response).to receive(:is_a?).with(Net::HTTPSuccess).and_return(true)
+        expect(http_response).to receive(:body).and_return(response_body)
 
         result = described_class.new.call(search: "rails", page: 2)
+        expect(result).to eq(JSON.parse(response_body))
+      end
+    end
 
-        expect(result).to eq(response_body)
+    context "with error response" do
+      it "raises an error" do
+        uri = URI("https://rubygems.org/api/v1/search.json")
+        uri.query = URI.encode_www_form(query: "rails", page: 1)
+
+        expect(Net::HTTP).to receive(:get_response).with(uri).and_return(http_response)
+        expect(http_response).to receive(:is_a?).with(Net::HTTPSuccess).and_return(false)
+        expect(http_response).to receive(:code).and_return("500")
+
+        expect {
+          described_class.new.call(search: "rails")
+        }.to raise_error(/RubyGems API request failed/)
       end
     end
   end
